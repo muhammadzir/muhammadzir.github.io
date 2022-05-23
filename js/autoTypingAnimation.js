6f4312e054e7c3a0767bee69e0dc94ff1444af5d
M Wazirus Sultoni <42231111+muhammadzir@users.noreply.github.com>
2022-05-23 13:01:35 +0700

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am M Wazirus Sultoni (Muhammad Zir)</span> Saya berasal dari Pringsewu, Lampung, ID"
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
